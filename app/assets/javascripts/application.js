ab1cb95019df1b27734c0ad7c79c86e77c51e422
boris huruguen <boris.huruguen@gmail.com>
2015-12-01 14:32:02 +0100

    structure de base front-end ajouté

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,24 @@
+//= require cart
+//= raquire navbar
+//= cbpFWTabs
+
+
+
+
+
+
+//= modernizr.custom
+
+//= require bootstrap
+//= require bootstrap.min
 //= require jquery
 //= require jquery_ujs
+
+
+
+
 //= require bootstrap-sprockets
+//= require navbarbootsnip
+//= require cart
 //= require_tree .
+
